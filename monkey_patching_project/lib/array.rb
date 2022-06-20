4395e9f09fda02fe270c5bfd9ea489a9765f4bd0
Derek <liderek63@gmail.com>
2022-06-20 15:06:15 -0700

    completed part_1

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -1,4 +1,34 @@
 # Monkey-Patch Ruby's existing Array class to add your own custom methods
 class Array
-  
+    def span
+        return nil if self.empty?
+        return self.max - self.min
+    end
+
+    def average
+        return nil if self.empty?
+        return self.sum / self.length.to_f
+    end
+
+    def median
+        return nil if self.empty?
+        middle = self.length / 2
+        if self.length.odd?
+            return self.sort[middle]
+        else
+            return (self.sort[middle] + self.sort[middle - 1]) / 2.0
+        end
+    end
+
+    def counts
+        count = Hash.new(0)
+        self.each do |i|
+            count[i] += 1
+        end
+        return count
+    end
+
+    def my_count
+    end
+
 end
