bf326a290fd06988894eac4b7e6dd344db081565
Derek <liderek63@gmail.com>
2022-06-20 15:42:18 -0700

    completed part_2

--- monkey_patching_project/lib/array.rb
+++ monkey_patching_project/lib/array.rb
@@ -28,7 +28,36 @@ class Array
         return count
     end
 
-    def my_count
+    def my_count(ele)
+        return self.select { |el| el == ele}.length
     end
 
+    def my_index(ele)
+        self.each_with_index do |el, i|
+            if el == ele
+                return i
+            end
+        end
+        return nil
+    end
+
+    def my_uniq
+        uniq = []
+        self.each do |ele|
+            if !uniq.include?(ele)
+                uniq << ele
+            end
+        end
+        return uniq
+    end
+
+    def my_transpose
+        transpose = Array.new(self.length) { Array.new(self.length) }
+        (0...self.length).each do |row|
+            (0...self.length).each do |col|
+                transpose[row][col] = self[col][row]
+            end
+        end
+        return transpose
+    end
 end
