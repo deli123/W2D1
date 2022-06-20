480ea7e51c9065a720908ab0a0445d55984c571a
Derek <liderek63@gmail.com>
2022-06-20 14:37:28 -0700

    completed bootcamp.rb

--- bootcamp_project/lib/bootcamp.rb
+++ bootcamp_project/lib/bootcamp.rb
@@ -1,3 +1,65 @@
 class Bootcamp
-  
+    def initialize(name, slogan, student_capacity)
+        @name = name
+        @slogan = slogan
+        @student_capacity = student_capacity
+        @teachers = []
+        @students = []
+        @grades = Hash.new { |h,k| h[k] = Array.new }
+    end
+
+    def name
+        @name
+    end
+
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher)
+        @teachers << teacher
+    end
+
+    def enroll(student)
+        if @students.length < @student_capacity
+            @students << student
+            return true
+        else
+            return false
+        end
+    end
+
+    def enrolled?(student)
+        return @students.include?(student)
+    end
+
+    def student_to_teacher_ratio
+        return @students.length / @teachers.length
+    end
+
+    def add_grade(student, grade)
+        if enrolled?(student)
+            @grades[student] << grade
+            return true
+        else
+            return false
+        end
+    end
+
+    def num_grades(student)
+        return @grades[student].length
+    end
+
+    def average_grade(student)
+        return nil if @grades[student].empty? || !enrolled?(student) 
+        return @grades[student].sum / @grades[student].length
+    end
 end
